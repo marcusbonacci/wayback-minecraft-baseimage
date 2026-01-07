b8b794f3d70eb8dc1d10a97d54fbf18130dd2126
marcusbonacci <marcusbonacci+github@gmail.com>
2026-01-06 21:29:01 -0800

    Add: Added config, requires accessors and cleanup.

--- src/main/java/net/gwaii/Main.java
+++ src/main/java/net/gwaii/Main.java
@@ -1,29 +1,37 @@
 package net.gwaii;
 
+import net.gwaii.config.ServerConfig;
 import net.gwaii.events._AsyncPlayerConfigurationEvent;
 import net.gwaii.events._PlayerSpawnEvent;
 import net.minestom.server.Auth;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
-import net.minestom.server.instance.LightingChunk;
-import net.minestom.server.instance.block.Block;
-import net.minestom.server.world.biome.Biome;
+
+import java.util.Map;
+import java.util.Objects;
 
 public class Main {
 
+    private Map<String, Object> config;
+    private Integer port;
+    private String motd;
 
     void main() {
         var server = initializeServer();
 
-        server.start("0.0.0.0", 25565);
+        server.start("0.0.0.0", port);
     }
 
     MinecraftServer initializeServer() {
         var server = MinecraftServer.init(new Auth.Online());
 
+        // Messy, fix with accessors and helpers
+        config = ServerConfig.getConfig().config;
+        port = (Integer) config.get("SERVER_PORT");
+        motd = (String) config.get("SERVER_MOTD");
+
         var globalEventHandler = MinecraftServer.getGlobalEventHandler();
-        var instanceManager = MinecraftServer.getInstanceManager();
 
         var worldLoader = new WorldLoader();
         // Setup world repository
